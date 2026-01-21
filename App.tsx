15bd2041aee2712b23c122bc32f626dfaf6b9c77
ponciart <g.ponciano8@gmail.com>
2026-01-21 18:54:21 -0300

    Fix wrong Layout import

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from '../Layout';
+import Layout from './Layout';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import POS from './pages/POS';
